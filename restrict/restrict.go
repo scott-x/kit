0bb144aec593a87a2c1aa6eac4fb02f11529ae51
scott-x <mbp98k@gmail.com>
2023-03-26 10:07:12 +0800

    locate(%s , %s) ==>locate('%s' , %s)

--- restrict/restrict.go
+++ restrict/restrict.go
@@ -127,9 +127,9 @@ func HandleDbFiles(db_fields []*DBField) ([]interface{}, string) {
 				}
 			case LOCATE:
 				if num_of_valid_condition == 0 {
-					condition += fmt.Sprintf(` locate(%s , %s)>0 `, value.(string), fieldName)
+					condition += fmt.Sprintf(` locate('%s' , %s)>0 `, value.(string), fieldName)
 				} else {
-					condition += fmt.Sprintf(` and locate(%s , %s)>0 `, value.(string), fieldName)
+					condition += fmt.Sprintf(` and locate('%s' , %s)>0 `, value.(string), fieldName)
 				}
 			}
 			num_of_valid_condition++
